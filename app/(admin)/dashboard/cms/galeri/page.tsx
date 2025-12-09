a0fadca403ebf25b944760b7f6877758b54b20d6
rexxy04 <muhammadirgi088@gmail.com>
2025-12-09 21:27:47 +0800

    mempersiapkan page untuk menu sidebar admin

---
+++ app/(admin)/dashboard/cms/galeri/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-slate-800 mb-4">Judul Halaman</h1>
+      <div className="p-8 bg-white rounded-xl border border-slate-200 text-center text-slate-500">
+        <p>Area Konten Data Ril (Menunggu Integrasi Firestore)</p>
+      </div>
+    </div>
+  );
+}
