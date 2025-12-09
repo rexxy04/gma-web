f1e6540831a4af35b2d74b17fdf275043cff843e
rexxy04 <muhammadirgi088@gmail.com>
2025-12-09 21:44:54 +0800

    buat page, query index pada page tambah warga

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { 
   LayoutDashboard, 
   Users, 
